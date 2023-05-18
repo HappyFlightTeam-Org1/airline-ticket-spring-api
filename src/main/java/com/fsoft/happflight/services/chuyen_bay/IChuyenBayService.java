288a2f11cdb7455ae41b63038bf1d9f5e9f439c3
nguyenhuuduc3888 <nguyenhuuduc3888@gmail.com>
2023-05-18 08:08:22 +0700

    up bai

--- src/main/java/com/fsoft/happflight/services/chuyen_bay/IChuyenBayService.java
+++ src/main/java/com/fsoft/happflight/services/chuyen_bay/IChuyenBayService.java
@@ -1,15 +1,22 @@
 package com.fsoft.happflight.services.chuyen_bay;
 
-import com.fsoft.happflight.entities.chuyen_bay.ChuyenBay;
-
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import com.fsoft.happflight.entities.chuyen_bay.ChuyenBay;
 
 public interface IChuyenBayService {
 
-    void save(ChuyenBay chuyenBay);
+	void save(ChuyenBay chuyenBay);
 
-    List<ChuyenBay> finAll();
-    
-    ChuyenBay findById(String maChuyenBay);
+	List<ChuyenBay> finAll();
+
+	ChuyenBay findById(String maChuyenBay);
+
+	Page<ChuyenBay> searchChuyenBay(String diemDi, String diemDen, String ngayKhoiHanh,
+			Sort.Direction sortDirection, String sortBy, Pageable pageable);
 
 }
