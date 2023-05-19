d28f2cf6c72135ba24a7e87158bac57823e543cf
Thienduy0195 <thienduy0195@gmail.com>
2023-05-19 10:02:01 +0700

    DuyNT update ve-may-bay test create

--- src/main/java/com/fsoft/happflight/controllers/TestRest.java
+++ src/main/java/com/fsoft/happflight/controllers/TestRest.java
@@ -1,16 +1,25 @@
 package com.fsoft.happflight.controllers;
 
+import com.fsoft.happflight.config.ModelMapperClass;
+import com.fsoft.happflight.dto.chuyen_bay.ChuyenBayDTO;
+import com.fsoft.happflight.entities.chuyen_bay.ChuyenBay;
+import com.fsoft.happflight.entities.chuyen_bay.HangBay;
 import com.fsoft.happflight.entities.chuyen_bay.MayBay;
 import com.fsoft.happflight.entities.dat_cho.Ghe;
+import com.fsoft.happflight.entities.hanh_khach.HanhKhach;
+import com.fsoft.happflight.entities.hoa_don.HoaDon;
+import com.fsoft.happflight.entities.nguoi_dung.NguoiDung;
+import com.fsoft.happflight.repositories.nguoi_dung.INguoiDungRepository;
 import com.fsoft.happflight.services.chuyen_bay.IMayBayService;
+import com.fsoft.happflight.services.dat_cho.IDatChoService;
 import com.fsoft.happflight.services.dat_cho.IGheService;
+import com.fsoft.happflight.services.hanh_khach.IHanhKhachService;
+import com.fsoft.happflight.services.hoa_don.IHoaDonService;
+import com.fsoft.happflight.services.ve_may_bay.IVeMayBayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +29,21 @@ import java.util.List;
 public class TestRest {
 
     private IMayBayService mayBayService;
+
+    @Autowired
+    private IVeMayBayService veMayBayService;
+
+    @Autowired
+    private INguoiDungRepository nguoiDungRepository;
+
+    @Autowired
+    private IHanhKhachService hanhKhachService;
+
+    @Autowired
+    private IDatChoService datChoService;
+
+    @Autowired
+    private IHoaDonService hoaDonService;
 
     @Autowired
     private IGheService gheService;
@@ -45,5 +69,17 @@ public class TestRest {
             return new ResponseEntity<>(ghes, HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(ghes, HttpStatus.OK);
+    }
+
+    @PostMapping("/saveVeMayBay")
+    public ResponseEntity<String> saveTicket(@RequestBody ChuyenBayDTO chuyenBayDTO) {
+        try {
+//        NguoiDung nguoiDung = nguoiDungRepository.findById("admin@gmail.com").orElse(null);
+            HoaDon hoaDon = hoaDonService.findById("OD23JS524");
+//        HanhKhach hanhKhach = hanhKhachService.saveHanhKhach()
+            return ResponseEntity.ok("Thêm thành công!");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Thêm thất bại!");
+        }
     }
 }
