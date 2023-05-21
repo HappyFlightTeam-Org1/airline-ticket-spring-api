7baa739320f31f4b0749cd984ca2ca9d44a6ba04
Thienduy0195 <thienduy0195@gmail.com>
2023-05-21 18:13:58 +0700

    DuyNT58 update thanh toan VNPay

--- src/main/java/com/fsoft/happflight/services/nguoi_dung/impl/NguoiDungServiceImpl.java
+++ src/main/java/com/fsoft/happflight/services/nguoi_dung/impl/NguoiDungServiceImpl.java
@@ -1,7 +1,9 @@
 package com.fsoft.happflight.services.nguoi_dung.impl;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope("prototype")
 public class NguoiDungServiceImpl {
 }
