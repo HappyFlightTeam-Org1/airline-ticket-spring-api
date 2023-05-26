c1cb5bcf1fd9b16a5c5c1075bec13cdec95a1c9a
sog-spirit <78286856+sog-spirit@users.noreply.github.com>
2023-05-26 10:25:54 +0700

    modified saveThayDoiNguoiDung; added getAllQuocTich;

--- src/main/java/com/fsoft/happflight/services/nguoi_dung/INguoiDungAuthenService.java
+++ src/main/java/com/fsoft/happflight/services/nguoi_dung/INguoiDungAuthenService.java
@@ -2,13 +2,16 @@ package com.fsoft.happflight.services.nguoi_dung;
 
 import com.fsoft.happflight.dto.nguoi_dung.DangKyDTO;
 import com.fsoft.happflight.dto.nguoi_dung.ThayDoiThongTinNguoiDungDTO;
+import com.fsoft.happflight.entities.nguoi_dung.NguoiDung;
 
 public interface INguoiDungAuthenService {
     boolean validateEmail(String email);
 
     void saveNguoiDung(DangKyDTO dangKyDTO);
 
-    void saveThayDoiNguoiDung(ThayDoiThongTinNguoiDungDTO thayDoiThongTinNguoiDungDTO);
+    boolean saveThayDoiNguoiDung(NguoiDung nguoiDung, ThayDoiThongTinNguoiDungDTO thayDoiThongTinNguoiDungDTO);
 
     String getUsernameFromNguoiDung(String email);
+
+    NguoiDung getWithUsername(String username);
 }
