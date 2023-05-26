7a95f90093e9d0e40338a9c6d68c34dc49258d1c
dmy226610 <dmy226610@gmail.com>
2023-05-26 18:42:55 +0700

    duyen chat

--- src/main/java/com/fsoft/happflight/controllers/ChatController.java
+++ src/main/java/com/fsoft/happflight/controllers/ChatController.java
@@ -1,23 +1,19 @@
 package com.fsoft.happflight.controllers;
 
 import com.fsoft.happflight.entities.chat.Message;
-import com.fsoft.happflight.entities.dat_cho.DatCho;
-import com.fsoft.happflight.entities.hanh_khach.HanhKhach;
+import com.fsoft.happflight.entities.chat.NewMessage;
 import com.fsoft.happflight.services.chat.IChatService;
+import com.fsoft.happflight.services.chat.INewMassageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @RestController
 @CrossOrigin("*")
@@ -30,35 +26,88 @@ public class ChatController {
     @Autowired
     IChatService iChatService;
 
+    @Autowired
+    INewMassageService iNewMassageService;
+
     @MessageMapping("/message")
-    public Message recMessage(@Payload Message message){
-        simpMessagingTemplate.convertAndSendToUser(message.getReciptient(),"/queue/messages",message);
-        simpMessagingTemplate.convertAndSendToUser(message.getSender(),"/queue/messages",message);
+    public Message recMessage(@Payload Message message) {
+        simpMessagingTemplate.convertAndSendToUser(message.getReciptient(), "/queue/messages", message);
+        simpMessagingTemplate.convertAndSendToUser(message.getSender(), "/queue/messages", message);
         message.getSender();
         System.out.println(message.toString());
         message.setTime(LocalDateTime.now());
         iChatService.Save(message);
         return message;
     }
+
     @GetMapping(value = "/user")
-    public List<String> getUser(){
+    public List<String> getUser() {
         List<Message> messages = iChatService.FindALL();
-        Set<String> uniqueSenders = new HashSet<>();
+        Collections.sort(messages, Comparator.comparing(Message::getTime));
+        System.out.println(messages);
+        Collections.reverse(messages);
+        Set<String> uniqueSenders = new LinkedHashSet<>();
         for (Message message : messages) {
             uniqueSenders.add(message.getSender());
         }
         List<String> senders = new ArrayList<>(uniqueSenders);
-
+        System.out.println(senders);
         return senders;
     }
 
-    @GetMapping(value = "/getchat")
-    public List<Message> getMessageUser(@RequestParam(name = "user") String user){
+    @GetMapping(value = "/new-message")
+    public List<NewMessage> getAllNewMessage() {
+        List<NewMessage> users = iNewMassageService.findAll();
+        System.out.println(users);
+        return users;
+    }
 
+    @DeleteMapping(value = "/delete-new-message/{user}")
+    public ResponseEntity<String> deleteNewMessage(@PathVariable String user) {
+        List<NewMessage> messages = iNewMassageService.findByUserContainingIgnoreCase(user);
+        if (null != messages) {
+            for (NewMessage message : messages) {
+                iNewMassageService.delete(message);
+            }
+        } else {
+            System.out.println("Null roi");
+        }
+        String messageOk = "Đã xóa user: " + user;
+        return ResponseEntity.ok().body(messageOk);
+    }
+
+    @GetMapping(value = "/save-new-message/{user}")
+    public ResponseEntity<String> saveNewMessage(@PathVariable String user) {
+        boolean check = true;
+        List<NewMessage> newMessages = iNewMassageService.findAll();
+        System.out.println(newMessages);
+        for (NewMessage message : newMessages) {
+            if (message.getUser().equals(user)) {
+                int k = message.getQuatity();
+                System.out.println(k);
+                message.setQuatity(k+1);
+                iNewMassageService.save(message);
+                check = false;
+                break;
+            }
+        }
+        if (check) {
+            NewMessage newMessage = new NewMessage();
+            newMessage.setUser(user);
+            newMessage.setQuatity(1);
+            iNewMassageService.save(newMessage);
+        }
+        String messageOk = "Đã theem user: " + user;
+        return ResponseEntity.ok().body(messageOk);
+    }
+
+
+    @GetMapping(value = "/getchat")
+    public List<Message> getMessageUser(@RequestParam(name = "user") String user) {
         List<Message> messages = iChatService.FindByUser(user);
         Collections.sort(messages, Comparator.comparing(Message::getTime));
-        if(user==""){
-            messages=null;
+        if (user == "") {
+            messages = null;
         }
         return messages;
     }
