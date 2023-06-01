2388ae6bac6fa24264d22ca5646e371c609e235e
Thienduy0195 <thienduy0195@gmail.com>
2023-06-01 16:41:23 +0700

    DuyNT58 update ddl, dml project

---
+++ src/main/java/com/fsoft/happflight/dto/ve_may_bay/IVeMayBayDTO.java
@@ -0,0 +1,11 @@
+package com.fsoft.happflight.dto.ve_may_bay;
+
+public interface IVeMayBayDTO {
+    String getMaVe();
+    String getTenHanhKhach();
+    String getNgayKhoiHanh();
+    String getDiemDi();
+    String getDiemDen();
+    String getHangVe();
+    Long getGiaVe();
+}
